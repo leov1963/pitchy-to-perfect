88b411ede3528e65bc0f868f0a51c4e1bae71462
leov1963 <leov1963@gmail.com>
2021-03-31 16:23:40 -0700

    Add [users]admin.py - add CustomUserAdmin class for creating admin users

--- pitchy_to_perfect_backend/users/admin.py
+++ pitchy_to_perfect_backend/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):    
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email']
+admin.site.register(CustomUser, CustomUserAdmin)

64113325e88ee49848ad0019a63169ea027dc25e
leov1963 <leov1963@gmail.com>
2021-03-31 20:46:20 -0700

    import & add login & signup to App.js

--- pitchy-to-perfect-frontend/src/App.js
+++ pitchy-to-perfect-frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Login from './views/auth/Login'; // new 
+import Signup from './views/auth/Signup'; // new
 
 const App = () => {
   return (
@@ -7,6 +9,8 @@ const App = () => {
       <Router>
         <Navbar />
         <Switch>
+          <Route path='/login' component={Login} exact />
+          <Route path='/signup' component={Signup} exact />
         </Switch>
       </Router>
     </div>

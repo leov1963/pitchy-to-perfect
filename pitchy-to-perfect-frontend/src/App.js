53f07c26f7373f430f09cf308d49063a1c6d4ff8
leov1963 <leov1963@gmail.com>
2021-04-01 00:41:48 -0700

    Update App.js - Add/import logout and game routes

--- pitchy-to-perfect-frontend/src/App.js
+++ pitchy-to-perfect-frontend/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Login from './views/auth/Login'; // new 
-import Signup from './views/auth/Signup'; // new
+import Login from './views/auth/Login';
+import Signup from './views/auth/Signup';
+import Logout from './views/auth/Logout'; 
+import Game from './views/app/Game'; 
 
 const App = () => {
   return (
@@ -9,8 +11,10 @@ const App = () => {
       <Router>
         <Navbar />
         <Switch>
+          <Route path='/game' component={Game} exact />
           <Route path='/login' component={Login} exact />
           <Route path='/signup' component={Signup} exact />
+          <Route path='/logout' component={Logout} exact />
         </Switch>
       </Router>
     </div>

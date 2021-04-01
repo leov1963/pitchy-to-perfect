a8942e44270db55f64b71e79f52d03bb38a0980a
leov1963 <leov1963@gmail.com>
2021-03-31 17:11:18 -0700

    Add [components][layout]Navbar.js - Add basic nav bar that only links to game if user is logged in

---
+++ pitchy-to-perfect-frontend/src/components/layout/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const [isAuth, setIsAuth] = useState(false);
+
+    useEffect(() => {
+        if (localStorage.getItem('token') !== null) {
+            setIsAuth(true);
+        }
+    }, []);
+
+    return (
+        <nav>
+        <h1>Django React Auth</h1>
+        <ul>
+            {isAuth === true ? (
+            <Fragment>
+                {' '}
+                <li>
+                <Link to='/game'>Dashboard</Link>
+                </li>
+                <li>
+                <Link to='/logout'>Logout</Link>
+                </li>
+            </Fragment>
+            ) : (
+            <Fragment>
+                {' '}
+                <li>
+                <Link to='/login'>Login</Link>
+                </li>
+                <li>
+                <Link to='/signup'>Signup</Link>
+                </li>
+            </Fragment>
+            )}
+        </ul>
+        </nav>
+    );
+};
+
+export default Navbar;

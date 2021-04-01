2ba1871417e119a1dc99f49e25c720920a6fbe11
leov1963 <leov1963@gmail.com>
2021-03-31 19:54:15 -0700

    Update App.js - import and add Navbar

--- pitchy-to-perfect-frontend/src/App.js
+++ pitchy-to-perfect-frontend/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
+        <Navbar />
         <Switch>
         </Switch>
       </Router>

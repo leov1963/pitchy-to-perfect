655f66f7d8da68cec32ac02cc574a80a2aea77aa
leov1963 <leov1963@gmail.com>
2021-04-01 21:32:41 -0700

    Update Game.js - add random note function - import Tone to generate tone for each note - add play note functionality

--- pitchy-to-perfect-frontend/src/views/app/Game.js
+++ pitchy-to-perfect-frontend/src/views/app/Game.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import useSound from 'use-sound';
+// import useSound from 'use-sound';
+import * as Tone from 'tone'
+// import cNote from '../../assets/C-Note.mp3'
 import './game.css'
 
 const Game = () => {
@@ -7,29 +9,62 @@ const Game = () => {
     const [loading, setLoading] = useState(true);
     const [isActive, setIsActive] = useState(false);
 
+    let isAnswerLeft = false;
+
+    const synth = new Tone.Synth().toDestination();
+    
     useEffect(() => {
         if (localStorage.getItem('token') === null) {
-        window.location.replace('http://localhost:3000/login');
+            window.location.replace('http://localhost:3000/login');
         } else {
-        fetch('http://127.0.0.1:8000/api/v1/users/auth/user/', {
-            method: 'GET',
-            headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Token ${localStorage.getItem('token')}`
-            }
-        })
+            fetch('http://127.0.0.1:8000/api/v1/users/auth/user/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Token ${localStorage.getItem('token')}`
+                }
+            })
             .then(res => res.json())
             .then(data => {
-            setUserEmail(data.email);
-            setLoading(false);
+                setUserEmail(data.email);
+                setLoading(false);
             });
         }
     }, []);
 
     const startGame = () => {
-        console.log("Hello!!!")
         setIsActive(!isActive);
     };
+    
+    const myNotes = [
+        "C4",
+        "C#4",
+        "D4",
+        "D#4",
+        "E4",
+        "F4",
+        "F#4",
+        "G4",
+        "G#4",
+        "A4",
+        "A#4",
+        "B4"
+    ];
+
+    const randomNote = myNotes[Math.floor(Math.random()*myNotes.length)];
+    
+    
+
+    
+    const randomNote2 = myNotes[Math.floor(Math.random()*myNotes.length)];
+
+    
+    const PlayNote = () => {
+        synth.triggerAttackRelease(randomNote, "8n");
+        console.log(randomNote)
+    };
+    
+    
 
     return (
         <div>
@@ -39,16 +74,14 @@ const Game = () => {
             <div>
                 <button onClick={startGame} className={`start-btn ${isActive ? "hidden" : ""}`} id="start">Start!</button>
                 <div className={`play-note-btn ${!isActive ? "hidden" : ""}`}>
-                    <button>🔊</button>
-                    replay note
+                    <button onClick={PlayNote}>🔊</button>
+                    play note
                 </div>
             </div>
             <br />
             <div>
-                <button className={`note-btn ${!isActive ? "hidden" : ""}`} id="note-btn">♪</button>
-                <button className={`note-btn ${!isActive ? "hidden" : ""}`} id="note-btn">♪</button>
-                <button className={`note-btn ${!isActive ? "hidden" : ""}`} id="note-btn">♪</button>
-                <button className={`note-btn ${!isActive ? "hidden" : ""}`} id="note-btn">♪</button>
+                <button className={`note-btn ${!isActive ? "hidden" : ""}`} id="note-btn">{`${isAnswerLeft ?randomNote : randomNote2 }`}</button>
+                <button className={`note-btn ${!isActive ? "hidden" : ""}`} id="note-btn">{`${isAnswerLeft ?randomNote2 : randomNote }`}</button>
             </div>
             </Fragment>
         )}

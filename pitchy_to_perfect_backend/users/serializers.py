69f42163c3cde08c4e854e079c74d056e2f64ea0
leov1963 <leov1963@gmail.com>
2021-04-05 11:50:15 -0700

    Update seializers.py: Add ScoreSerializer

--- pitchy_to_perfect_backend/users/serializers.py
+++ pitchy_to_perfect_backend/users/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework.serializers import ModelSerializer, ReadOnlyField
-from .models import CustomUser
+from .models import CustomUser, HighScore
 class UserSerializer(ModelSerializer):
     highest_score = ReadOnlyField()
     class Meta:
         model = CustomUser
         fields = ('email', 'username', 'highest_score')
+
+class ScoreSerializer(ModelSerializer):
+
+    class Meta:
+        model = HighScore
+        fields = ('score', 'date', 'user')

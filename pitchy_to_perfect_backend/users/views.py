66dfe9387a505b42402df5f1957afbe37431eabd
leov1963 <leov1963@gmail.com>
2021-04-05 11:51:13 -0700

    update views.py: - Add score view set

--- pitchy_to_perfect_backend/users/views.py
+++ pitchy_to_perfect_backend/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import HighScore
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, ScoreSerializer
 
 # Create your views here.
+class ScoreViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = HighScore.objects.all()
+    serializer_class = ScoreSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]

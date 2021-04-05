4a4646fef5a7bdcae88ccb2f273b142cce8c513a
leov1963 <leov1963@gmail.com>
2021-04-05 11:53:05 -0700

    update users/urls.py: - Add "" path to include router.urls

--- pitchy_to_perfect_backend/users/urls.py
+++ pitchy_to_perfect_backend/users/urls.py
@@ -1,5 +1,12 @@
 from django.urls import include, path
+from rest_framework import routers
+from users import views
+
+router = routers.DefaultRouter()
+router.register(r'scores', views.ScoreViewSet)
+
 urlpatterns = [
-path('auth/', include('rest_auth.urls')),    
-path('auth/register/', include('rest_auth.registration.urls'))
+    path('', include(router.urls)),
+    path('auth/', include('rest_auth.urls')),    
+    path('auth/register/', include('rest_auth.registration.urls'))
 ]

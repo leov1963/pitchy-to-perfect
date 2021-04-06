925638061900595ab18a066dece4c34f95eca426
leov1963 <leov1963@gmail.com>
2021-04-05 21:43:53 -0700

    Update settings.py: - update for heroku deployment

--- pitchy_to_perfect_backend/pitchy_to_perfect_backend/settings.py
+++ pitchy_to_perfect_backend/pitchy_to_perfect_backend/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
+import os
 
 from pathlib import Path
-import os
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -173,3 +174,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())

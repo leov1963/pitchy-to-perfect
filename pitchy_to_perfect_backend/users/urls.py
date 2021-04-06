9cafdfceaa1e6448574a233cbf49f4f952463f38
leov1963 <leov1963@gmail.com>
2021-04-05 21:05:51 -0700

    Update users/urls.py: - add scores/get route

--- pitchy_to_perfect_backend/users/urls.py
+++ pitchy_to_perfect_backend/users/urls.py
@@ -6,6 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'scores', views.ScoreViewSet)
 
 urlpatterns = [
+    path('scores/get', views.scores_list),
     path('', include(router.urls)),
     path('auth/', include('rest_auth.urls')),    
     path('auth/register/', include('rest_auth.registration.urls'))

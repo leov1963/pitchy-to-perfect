af1b0401e22aeed02849cd9d1220500007321d5b
leov1963 <leov1963@gmail.com>
2021-04-06 09:40:48 -0700

    Update views.py: - remove old comments

--- pitchy_to_perfect_backend/users/views.py
+++ pitchy_to_perfect_backend/users/views.py
@@ -10,19 +10,15 @@ class ScoreViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
-
     queryset = HighScore.objects.all()
     serializer_class = ScoreSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 def scores_list(request):
-    # res = requests.get(f"https://www.thecocktaildb.com/api/json/v1/{ API_KEY }/search.php?s=margarita")
-
 
     high_scores = HighScore.objects.order_by('-score')[:25]
     res = {'high_scores': []}
     for score in high_scores:
-        # username = CustomUser.objects.get(id=score.user).username
         serialized_score = {
             'date': score.date,
             'score': score.score,

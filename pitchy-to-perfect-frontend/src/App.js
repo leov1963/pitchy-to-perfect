b197fae45efc666a00eaaae0d6bb00e6b12d2992
leov1963 <leov1963@gmail.com>
2021-04-06 02:39:46 -0700

    Update App.js: - Add landing page & 404 page

--- pitchy-to-perfect-frontend/src/App.js
+++ pitchy-to-perfect-frontend/src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Login from './views/auth/Login';
 import Signup from './views/auth/Signup';
 import Logout from './views/auth/Logout'; 
 import Game from './views/app/Game'; 
+import LandingPage from './views/app/LandingPage';
 import ScoreBoard from './views/app/ScoreBoard'
+import NotFoundPage from './views/app/NotFoundPage'
 
 const App = () => {
   return (
@@ -17,6 +19,9 @@ const App = () => {
           <Route path='/login' component={Login} exact />
           <Route path='/signup' component={Signup} exact />
           <Route path='/logout' component={Logout} exact />
+          <Route path='/' component={LandingPage} exact />
+          <Route path='/404' component={NotFoundPage} exact />
+          <Redirect to="/404" />
         </Switch>
       </Router>
     </div>

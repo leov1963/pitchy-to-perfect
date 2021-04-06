55f9458e720f02922bd9e4e209fd5bf0a7782926
leov1963 <leov1963@gmail.com>
2021-04-05 21:10:07 -0700

    Update views.py: - Add scores_list view

--- pitchy_to_perfect_backend/users/views.py
+++ pitchy_to_perfect_backend/users/views.py
@@ -1,13 +1,32 @@
-from .models import HighScore
+from .models import HighScore, CustomUser
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import UserSerializer, ScoreSerializer
+import requests
+from django.http import JsonResponse
 
 # Create your views here.
 class ScoreViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+
     queryset = HighScore.objects.all()
     serializer_class = ScoreSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+def scores_list(request):
+    # res = requests.get(f"https://www.thecocktaildb.com/api/json/v1/{ API_KEY }/search.php?s=margarita")
+
+
+    high_scores = HighScore.objects.order_by('-score')[:25]
+    res = {'high_scores': []}
+    for score in high_scores:
+        # username = CustomUser.objects.get(id=score.user).username
+        serialized_score = {
+            'date': score.date,
+            'score': score.score,
+            'username': score.user.username
+        }
+        res['high_scores'].append(serialized_score)
+    return JsonResponse(res)

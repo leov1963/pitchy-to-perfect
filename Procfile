006f56a6a82ff73f84c67ee7d72976e81ab40445
leov1963 <leov1963@gmail.com>
2021-04-14 17:45:11 -0700

    update for deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn pitchy_to_perfect_backend.wsgi --log-file -
+web: gunicorn pitchy-to-perfect.wsgi --log-file -
--- pitchy_to_perfect_backend/pitchy_to_perfect_backend/settings.py
+++ pitchy_to_perfect_backend/pitchy_to_perfect_backend/settings.py
@@ -39,9 +39,9 @@ REST_FRAMEWORK = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
